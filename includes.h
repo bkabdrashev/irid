00aef86fc2b05dd3d49e2611a7144880a7a5c700
bekzat <bkbdrsh@gmail.com>
2026-03-18 17:26:40 +0500

    fixed clangd diagnostics

---
+++ includes.h
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "common.c"
+#include "str.c"
+#include "lex.c"
+#include "parse.c"
--- main.c
+++ main.c
@@ -1,16 +1,4 @@
-#include <sys/types.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
-#include "common.c"
-#include "str.c"
-#include "lex.c"
-#include "parse.c"
+#include "includes.h"
 // #include "irgen.c"
 // #include "typeid.c"
 
